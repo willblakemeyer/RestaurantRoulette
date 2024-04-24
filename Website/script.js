c98903d82baaaa8b67539492dcaf8f144c0ef050
darkassassin557 <145591414+darkassassin557@users.noreply.github.com>
2024-04-24 11:06:08 -0500

    Button
    Added working buttons based off current location and location entered

---
+++ Website/script.js
@@ -0,0 +1,8 @@
+function helloWorld(){
+    var locationEntered = document.getElementById('locationEntered').value 
+    alert('Searching ' + locationEntered + ' for the best place to eat!')
+}
+
+function currentLocation(){
+    alert('Searching based on your current location...')
+}
