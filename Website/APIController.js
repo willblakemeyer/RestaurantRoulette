304bcdddd44fb5835c7294ecfb00106655791f92
red2922 <jinthavongsa1@gmail.com>
2024-04-24 11:03:35 -0500

    Heheh funny startup

--- Website/APIController.js
+++ Website/APIController.js
@@ -13,3 +13,60 @@ class APIController {
           });
     }
 }
+let map;
+let center;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  center = { lat: 37.4161493, lng: -122.0812166 };
+  map = new Map(document.getElementById("map"), {
+    center: center,
+    zoom: 11,
+    // ...
+  });
+  findPlaces();
+}
+
+async function findPlaces() {
+  const { Place } = await google.maps.importLibrary("places");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const request = {
+    textQuery: "Tacos in Mountain View",
+    fields: ["displayName", "location", "businessStatus"],
+    includedType: "restaurant",
+    locationBias: { lat: 37.4161493, lng: -122.0812166 },
+    isOpenNow: true,
+    language: "en-US",
+    maxResultCount: 8,
+    minRating: 3.2,
+    region: "us",
+    useStrictTypeFiltering: false,
+  };
+  //@ts-ignore
+  const { places } = await Place.searchByText(request);
+
+  if (places.length) {
+    console.log(places);
+
+    const { LatLngBounds } = await google.maps.importLibrary("core");
+    const bounds = new LatLngBounds();
+
+    // Loop through and get all the results.
+    places.forEach((place) => {
+      const markerView = new AdvancedMarkerElement({
+        map,
+        position: place.location,
+        title: place.displayName,
+      });
+
+      bounds.extend(place.location);
+      console.log(place);
+    });
+    map.fitBounds(bounds);
+  } else {
+    console.log("No results");
+  }
+}
+
+initMap();
